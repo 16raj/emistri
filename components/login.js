f6c113bbd9510ef0fc9c5ee8526216d6d8b43eb5
raj gupta <palash1691999@gmail.com>
2021-04-28 16:58:44 +0530

    emistri

---
+++ components/login.js
@@ -0,0 +1,111 @@
+import React, { Component, useState } from 'react';
+import {
+    Platform, StyleSheet, View, Text,
+    Image, TouchableOpacity, Alert, TextInput, 
+} from 'react-native';
+import { color } from 'react-native-reanimated';
+import Register from '../components/register'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+function Login({ navigation }) {
+   
+    const [username, setUserName] = useState('');
+    const [userpassword, setUserPassword] = useState('');
+    const [errortext, setErrortext] = useState('');
+ 
+    /* state = {
+         username: "",
+         password: ""
+     }*/
+    const handleSubmitPress = () => {
+        setErrortext('');
+        if (!username) {
+            alert('Please fill username');
+            return false;
+        }
+        if (!userpassword) {
+            alert('Please fill Password');
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    
+        /* validate_field = () => {
+             const  { username, password } = this.state
+             if (username == "") {
+                 alert("Username must not be empty")
+                 return false
+             }
+             else if (password == "") {
+                 alert("Password must not be empty")
+                 return false
+             }
+             return true
+         }*/
+     
+        const  making_api_call = () => {
+             if (handleSubmitPress()) {
+                if(username && userpassword){
+                    navigation.replace('DrawerNavigatorRoutes');
+                    alert("successfully login");
+                }
+             }
+         }
+
+
+
+
+        return (
+            <View style={{
+                width: "100%", height: "100%", justifyContent: "center",
+                alignSelf: "center", alignContent: "center", alignItems: "center", backgroundColor: "white"
+            }}>
+                <Image source={require('../assets/Logo.jpeg')}
+                    style={{ width: '30%', height: '30%', resizeMode: 'contain', marginTop: '5%' }} />
+
+                <TextInput placeholder={"username"}
+                    /* onChangeText={(value) => this.setState({ username: value })}*/
+                    onChangeText={(username) =>
+                        setUserName(username)
+                    }
+                    style={{ height: 42, width: "80%", borderBottomWidth: 1 }}
+                />
+                <TextInput placeholder={"Password"}
+                    /*  onChangeText={(value) => this.setState({ password: value })}*/
+                    onChangeText={(userpassword) =>
+                        setUserPassword(userpassword)
+                    }
+                    style={{ height: 42, width: "80%", borderBottomWidth: 1, marginTop: "10%" }}
+                />
+                <View style={{ marginTop: "10%", width: "80%" }}>
+                    <TouchableOpacity style={{
+                        borderWidth: 1, height: 42, width: "80%",
+                        justifyContent: "center", alignItems: "center", borderRadius: 40,
+                        backgroundColor: "black", alignSelf: "center"
+                    }}
+                        onPress={making_api_call}
+
+
+                    >
+                        <Text style={{ color: "white" }}>
+                            Log in
+                    </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ marginTop: 20, alignItems: 'center' }}   
+                    >
+                        <Text style={{ color: 'green' }}
+                            onPress={() => navigation.navigate('Auth')}>
+                            don't have any account? Register
+                    </Text>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+        );
+
+
+    
+}
+export default Login;
